1d2255fe484bb71d555c8f354269abcd2a2ac034
Egor <31863304+EgorOrachyov@users.noreply.github.com>
2018-04-08 18:15:26 +0300

    Added parsing of source map file
    Deleted redundant methods from decoder
    Added additional file class to separate work with input sources

--- Base64.java
+++ Base64.java
@@ -2,144 +2,41 @@ package SourceMap;
 
 import java.util.HashMap;
 
+/**
+ *  Base64 class introduces mentioned code table
+ *  with methods to encode and decode
+ *  sequences of chars
+ */
 public class Base64 {
+
     public Base64() {
         CharToInt = new HashMap<>();
         IntToChar = new HashMap<>();
 
         int alphabetSize = 26;
 
-        for(int i = 0; i < alphabetSize; i++) {
-            CharToInt.put(Character.valueOf('='), 0);
+        // Build first 26 chars and values (Upper case)
+        for (int i = 0; i < alphabetSize; i++) {
+            CharToInt.put((char) ((short) ('A') + i), i);
+            IntToChar.put(i, (char) ((short) ('A') + i));
         }
 
-        CharToInt.put('A', 0x00);
-        CharToInt.put('B', 0x01);
-        CharToInt.put('C', 0x02);
-        CharToInt.put('D', 0x03);
-        CharToInt.put('E', 0x04);
-        CharToInt.put('F', 0x05);
-        CharToInt.put('G', 0x06);
-        CharToInt.put('H', 0x07);
-        CharToInt.put('I', 0x08);
-        CharToInt.put('J', 0x09);
-        CharToInt.put('K', 0x0A);
-        CharToInt.put('L', 0x0B);
-        CharToInt.put('M', 0x0C);
-        CharToInt.put('N', 0x0D);
-        CharToInt.put('O', 0x0E);
-        CharToInt.put('P', 0x0F);
-        CharToInt.put('Q', 0x10);
-        CharToInt.put('R', 0x11);
-        CharToInt.put('S', 0x12);
-        CharToInt.put('T', 0x13);
-        CharToInt.put('U', 0x14);
-        CharToInt.put('V', 0x15);
-        CharToInt.put('W', 0x16);
-        CharToInt.put('X', 0x17);
-        CharToInt.put('Y', 0x18);
-        CharToInt.put('Z', 0x19);
-        CharToInt.put('a', 0x1A);
-        CharToInt.put('b', 0x1B);
-        CharToInt.put('c', 0x1C);
-        CharToInt.put('d', 0x1D);
-        CharToInt.put('e', 0x1E);
-        CharToInt.put('f', 0x1F);
-        CharToInt.put('g', 0x20);
-        CharToInt.put('h', 0x21);
-        CharToInt.put('i', 0x22);
-        CharToInt.put('j', 0x23);
-        CharToInt.put('k', 0x24);
-        CharToInt.put('l', 0x25);
-        CharToInt.put('m', 0x26);
-        CharToInt.put('n', 0x27);
-        CharToInt.put('o', 0x28);
-        CharToInt.put('p', 0x29);
-        CharToInt.put('q', 0x2A);
-        CharToInt.put('r', 0x2B);
-        CharToInt.put('s', 0x2C);
-        CharToInt.put('t', 0x2D);
-        CharToInt.put('u', 0x2E);
-        CharToInt.put('v', 0x2F);
-        CharToInt.put('w', 0x30);
-        CharToInt.put('x', 0x31);
-        CharToInt.put('y', 0x32);
-        CharToInt.put('z', 0x33);
-        CharToInt.put('0', 0x34);
-        CharToInt.put('1', 0x35);
-        CharToInt.put('2', 0x36);
-        CharToInt.put('3', 0x37);
-        CharToInt.put('4', 0x38);
-        CharToInt.put('5', 0x39);
-        CharToInt.put('6', 0x3A);
-        CharToInt.put('7', 0x3B);
-        CharToInt.put('8', 0x3C);
-        CharToInt.put('9', 0x3D);
+        // Build second 26 chars and values (Lower case)
+        for (int i = 0; i < alphabetSize; i++) {
+            CharToInt.put((char) ((short) ('a') + i), i + alphabetSize);
+            IntToChar.put(i + alphabetSize, (char) ((short) ('a') + i));
+        }
+
+        // Build numbers from 0 to 9
+        for (int i = 0; i < 10; i++) {
+            CharToInt.put((char)(i), i + 2 * alphabetSize);
+            IntToChar.put(i + 2 *  alphabetSize, (char)(i));
+        }
+
+        // Build arithmetic symbols
         CharToInt.put('+', 0x3E);
         CharToInt.put('/', 0x3F);
 
-        IntToChar.put(0x00, 'A');
-        IntToChar.put(0x01, 'B');
-        IntToChar.put(0x02, 'C');
-        IntToChar.put(0x03, 'D');
-        IntToChar.put(0x04, 'E');
-        IntToChar.put(0x05, 'F');
-        IntToChar.put(0x06, 'G');
-        IntToChar.put(0x07, 'H');
-        IntToChar.put(0x08, 'I');
-        IntToChar.put(0x09, 'J');
-        IntToChar.put(0x0A, 'K');
-        IntToChar.put(0x0B, 'L');
-        IntToChar.put(0x0C, 'M');
-        IntToChar.put(0x0D, 'N');
-        IntToChar.put(0x0E, 'O');
-        IntToChar.put(0x0F, 'P');
-        IntToChar.put(0x10, 'Q');
-        IntToChar.put(0x11, 'R');
-        IntToChar.put(0x12, 'S');
-        IntToChar.put(0x13, 'T');
-        IntToChar.put(0x14, 'U');
-        IntToChar.put(0x15, 'V');
-        IntToChar.put(0x16, 'W');
-        IntToChar.put(0x17, 'X');
-        IntToChar.put(0x18, 'Y');
-        IntToChar.put(0x19, 'Z');
-        IntToChar.put(0x1A, 'a');
-        IntToChar.put(0x1B, 'b');
-        IntToChar.put(0x1C, 'c');
-        IntToChar.put(0x1D, 'd');
-        IntToChar.put(0x1E, 'e');
-        IntToChar.put(0x1F, 'f');
-        IntToChar.put(0x20, 'g');
-        IntToChar.put(0x21, 'h');
-        IntToChar.put(0x22, 'i');
-        IntToChar.put(0x23, 'j');
-        IntToChar.put(0x24, 'k');
-        IntToChar.put(0x25, 'l');
-        IntToChar.put(0x26, 'm');
-        IntToChar.put(0x27, 'n');
-        IntToChar.put(0x28, 'o');
-        IntToChar.put(0x29, 'p');
-        IntToChar.put(0x2A, 'q');
-        IntToChar.put(0x2B, 'r');
-        IntToChar.put(0x2C, 's');
-        IntToChar.put(0x2D, 't');
-        IntToChar.put(0x2E, 'u');
-        IntToChar.put(0x2F, 'v');
-        IntToChar.put(0x30, 'w');
-        IntToChar.put(0x31, 'x');
-        IntToChar.put(0x32, 'y');
-        IntToChar.put(0x33, 'z');
-        IntToChar.put(0x34, '0');
-        IntToChar.put(0x35, '1');
-        IntToChar.put(0x36, '2');
-        IntToChar.put(0x37, '3');
-        IntToChar.put(0x38, '4');
-        IntToChar.put(0x39, '5');
-        IntToChar.put(0x3A, '6');
-        IntToChar.put(0x3B, '7');
-        IntToChar.put(0x3C, '8');
-        IntToChar.put(0x3D, '9');
         IntToChar.put(0x3E, '+');
         IntToChar.put(0x3F, '/');
     }
@@ -155,4 +52,18 @@ public class Base64 {
         return (IntToChar.containsKey(i) ? IntToChar.get(i) : '#');
     }
 
+    public String getDecodedBits(String s) {
+        StringBuilder result = new StringBuilder();
+
+        for (int index = 0; index < s.length(); index++) {
+            int code = getIntByChar(s.charAt(index));
+
+            // 5 + 1 is num of bits in one base64 point
+            for (int i = 5; i >= 0; i--) {
+                result.append(String.valueOf(1 & code >>> i));
+            }
+        }
+
+        return result.toString();
+    }
 }
